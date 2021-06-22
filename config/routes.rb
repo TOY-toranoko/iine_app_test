a0bfda2efcda7ebbe789f824dd26777e215f61c0
toy <19t2021j@shinshu-u.ac.jp>
2021-06-23 02:29:47 +0900

    いいね機能の追加
    index.html.erbとshow.html.erbの両方においていいね機能を使用できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 
+  post '/posts/:id', to:'likes#create', as: 'create_post_like'
   root 'posts#index'
-
+  
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :show, :create] do
     resources :likes, only: [:create, :destroy]
