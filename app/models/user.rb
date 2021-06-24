f78809cf53335a3572e2190808f98ce0f4f2ac3f
toy <19t2021j@shinshu-u.ac.jp>
2021-06-25 03:32:19 +0900

    routingの適正化
    routes.rbにresourceを使用するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # user -> posts
   has_many :posts, dependent: :destroy
+
+  # user -> likes
   has_many :likes, dependent: :destroy
+
+  # user -> likes -> posts
   has_many :liked_posts, through: :likes, source: :post
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  post 'like/:id' => 'likes#create', as: 'create_like'
-  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
-
-  post '/posts/:id', to:'likes#create', as: 'create_post_like'
   root 'posts#index'
   
   resources :users, only: [:index, :show]
+  resources :likes, only: [:destroy]
   resources :posts, only: [:index, :show, :create] do
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:create]
   end
   
-
 end
