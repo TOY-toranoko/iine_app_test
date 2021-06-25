9c8a542a5dff18bb95ed3077b231da36927bebcc
toy <19t2021j@shinshu-u.ac.jp>
2021-06-25 23:27:51 +0900

    非同期いいねの実装
    いいね機能をAjaxによって非同期で実装した

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,20 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id]) 
     Like.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to posts_path
+    render "posts/likes/create.js.erb"
   end
 
   def destroy
-    # Like.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
+    @post = Post.find(params[:post_id])
     Like.find_by(user_id: current_user.id, id: params[:id]).destroy
-    redirect_to posts_path
+    render "posts/likes/destroy.js.erb"
   end
+
+  def show
+    render partial: "posts/like", locals: { post: post}
+  end
+
+  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'posts#index'
   
   resources :users, only: [:index, :show]
-  resources :likes, only: [:destroy]
+  #resources :likes, only: [:destroy]
   resources :posts, only: [:index, :show, :create] do
-    resources :likes, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
   
 end
